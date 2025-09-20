a100ebb996882d3151b2d85df29c7d8f9638062f
Meraj Parsazad <merajparsazad@gmail.com>
2025-09-20 18:00:31 +0330

    Show user full name and avatar in UserAvatar.jsx with avatar images linked from public/avatar-images folder and db.json

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,13 +1,17 @@
+import { useAuth } from "./AuthContext";
+
 function UserAvatar() {
+  const { user } = useAuth();
+  const {fullName, avatar} = user
+
   return (
     <div className="flex min-w-40 items-center gap-4 text-xl font-medium text-gray-600">
       <img
-        src={null}
-        // alt={`Avatar of ${fullname}`}
-        alt=""
+        src={avatar || "/avatar-images/default-user.jpg"}
+        alt={`Avatar of ${fullName}`}
         className="block aspect-square w-14 rounded-[50%] object-cover object-center outline-2 outline-gray-100"
       />
-      <span className="text-nowrap">Full Name</span>
+      <span className="text-nowrap">{fullName}</span>
     </div>
   );
 }
